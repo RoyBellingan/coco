4e745e557413b61e4e4f1af3066435f2e4d02f21
Roy Bellingan <admin@seisho.us>
2018-03-17 02:01:02 +0000

    rusted geometry

--- coco.pro
+++ coco.pro
@@ -1,8 +1,8 @@
 QT -= gui
 
-CONFIG += c++11 console
+CONFIG += console
 CONFIG -= app_bundle
-
+CONFIG += c++1z
 # The following define makes your compiler emit warnings if you use
 # any feature of Qt which as been marked deprecated (the exact warnings
 # depend on your compiler). Please consult the documentation of the
@@ -15,3 +15,5 @@ DEFINES += QT_DEPRECATED_WARNINGS
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += main.cpp
+
+QMAKE_CXXFLAGS += -march=native
--- main.cpp
+++ main.cpp
@@ -5,31 +5,104 @@
 
 const uint8_t size = 5 ;
 
+unsigned char flipByte(unsigned char v) {
+	return ((v * 0x80200802ULL) & 0x0884422110ULL) * 0x0101010101ULL >> 32;
+}
 
 class Lato {
+private:
+	uint8_t sequence = 0;
 public:
-    Lato(const uint16_t val) {
-        static const uint16_t max = 255 >> ( 8 - size);
-        static const uint16_t maxBit = size - 1;
-        static const uint16_t minBit = 2;
-        auto bit = __builtin_popcount(val);
-        if(bit > maxBit || val > max){
-            printf("%i is too big (more than %i)!\n", val, max);
-            exit(1);
-        }
-        if(bit < minBit){
-            printf("%i is too small!\n", val);
-            exit(1);
-        }
-    }
+	Lato(){}
+	Lato(const uint8_t val) {
+		static const uint8_t max = 255 >> ( 8 - size);
+		static const uint8_t maxBit = size - 2;
+		static const uint8_t minBit = 2;
+		auto bit = __builtin_popcount(val);
+		if(bit > maxBit || val > max){
+			printf("%i is too big or not enought missing bits available ! \n", val);
+			exit(1);
+		}
+		if(bit < minBit){
+			printf("%i is too small!\n", val);
+			exit(1);
+		}
+		sequence = val;
+	}
+	void flip(){
+		static const uint8_t po = 8 - size;
+		sequence = flipByte (sequence) >> po;
+	}
+};
+
+enum Pos : u_int8_t{
+	top = 0,
+	left = 1,
+	bot = 2,
+	rigth = 3
 };
 
 class Quad {
+private:
+	Lato sides[4];
+	u_int8_t rotation = 0;
+	bool flipped = false;
 public:
-    Quad () {}
+	Quad (const Lato top, const Lato left, const Lato bot, const Lato rigth) {
+		sides[Pos::top] = top;
+		sides[Pos::bot] = bot;
+		sides[Pos::left]= left;
+		sides[Pos::rigth]= rigth;
+	}
+	Lato top(){
+		return sides[Pos::top + rotation];
+	}
+	Lato bot(){
+		return sides[(Pos::bot + rotation) % 4];
+	}
+	Lato rigth(){
+		return sides[(Pos::rigth + rotation) % 4];
+	}
+	Lato left(){
+		return sides[(Pos::left + rotation) % 4];
+	}
+
+	void rotate(uint8_t r = 0){
+		rotation += r ;
+		rotation = rotation % 4;
+	}
+
+	void flip(){
+		flipped = !flipped;
+		//top & down need flippin
+		sides[Pos::top + rotation].flip();
+		sides[(Pos::bot + rotation) % 4].flip();
+
+		//and left/right swap
+		Lato buf = sides[Pos::left];
+		sides[Pos::left] = sides[Pos::rigth];
+		sides[Pos::rigth] = buf;
+	}
+	bool getFlipped() const;
 };
 
-int main(int argc, char *argv[]) {
-    Lato a(24);
-    return 0;
+class Set{
+private:
+	Quad q1,q2,q3,q4,q5,q6;
+public:
+
+};
+
+#include <QElapsedTimer>
+
+int main(/*int argc, char *argv[]*/) {
+	Quad a(0b10110,0b10100,0b01110,0b11001);
+
+	a.flip ();
+
+	return 0;
+}
+
+bool Quad::getFlipped() const {
+	return flipped;
 }
